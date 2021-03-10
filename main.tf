eb3a79ac44e42b5f162e2be97afe8871ea20e9dc
Damian Sloane <damian@capacitorlabs.com>
2021-03-10 16:12:13 +1100

    Work in progress

--- main.tf
+++ main.tf
@@ -101,10 +101,10 @@ resource "opsgenie_integration_action" "alarm" {
     ignore_responders_from_payload = true
     entity = var.opsgenie_entity
     user = var.opsgenie_user
-    tags = merge([
+    tags = setunion(toset([
       "SQS",
       var.metric_name
-    ], var.opsgenie_tags)
+    ]), var.opsgenie_tags)
     priority = var.opsgenie_priority
     filter {
       type = "match-all"
