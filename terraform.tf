b0c1d57ed8438152a201732fa640116c2b3933c5
Damian Sloane <damian@capacitorlabs.com.au>
2021-06-20 18:24:40 +1000

    Work in progress

--- terraform.tf
+++ terraform.tf
@@ -2,37 +2,9 @@
 
 terraform {
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = ">= 3.1.0"
-    }
-    github = {
-      source = "hashicorp/github"
-      version = "~> 2.9.2"
-    }
-    local = {
-      source = "hashicorp/local"
-      version = "~> 1.4.0"
-    }
-    random = {
-      source = "hashicorp/random"
-      version = "~> 2.3.0"
-    }
-    opsgenie = {
-      source = "opsgenie/opsgenie"
-      version = "~> 0.5.2"
-    }
     sumologic = {
       source = "SumoLogic/sumologic"
-      version = "2.1.2"
-    }
-    template = {
-      source = "hashicorp/template"
-      version = "~> 2.1.2"
-    }
-    tls = {
-      source = "hashicorp/tls"
-      version = "~> 2.2.0"
+      version = "~> 2.9.3"
     }
   }
 }
